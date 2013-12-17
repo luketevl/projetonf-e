2c5f3b402957f94f8093806722963ddf75cc4695
luketevl <luketevl@6f5d0827-623d-b834-7d11-3d3fd3e998fb>
2013-12-17 20:23:06 +0000

    - Acrescentado FPDF - Conexão com BD - Consulta para DANFE

--- projetonf-e/ProjetoNfe/model/DataBase.php
+++ projetonf-e/ProjetoNfe/model/DataBase.php
@@ -6,54 +6,83 @@
  * @since 16/12/13
  * @version 1.0
  */
+// https://phpmyadmin.locaweb.com.br/
+     $server='186.202.152.100';
+      $user='portaldecifra15';
+      $pass='ikat1612';
+      $database='portaldecifra15';
+      $query=null;    
+    
+    
+    //conexão com o servidor
+    $conect = mysql_connect($server, $user, $pass);
+    
+    // Caso a conexão seja reprovada, exibe na tela uma mensagem de erro
+    if ($conect){
+    // Caso a conexão seja aprovada, então conecta o Banco de Dados.
+    $db = mysql_select_db($database);
+    echo '<h1>Conectado ao Banco de Dados!</h1>';
+    $query = 'select p.documento, p.nfe_numero , pro.titulo
+from pedidos p 
+join pedidos_itens ip on ip.pedido = p.id 
+join produtos pro on ip.produto = pro.id where p.documento="879" order by p.id;';
+    $result = mysql_query($query);
+    
+    $ar = array();
+    while ($row = mysql_fetch_assoc($result)) {
+    	$ar['documento']=$row['documento'];
+    	$ar['emitente']['nome']=$row['documento'];
+    	$ar['emitente']['endereco']=$row['documento'];
+    	$ar['emitente']['telefone']=$row['documento'];
 
-class DataBase{
+    	$ar['destinatario']['nome']=$row['documento'];
+    	$ar['destinatario']['endereco']=$row['documento'];
+    	$ar['destinatario']['telefone']=$row['documento'];
+    	$ar['destinatario']['cpf/cnpj']=$row['documento'];
+    	$ar['destinatario']['ie']=$row['documento'];
+    	
+    	$ar['transportadora']['nome']=$row['documento'];
+    	$ar['transportadora']['endereco']=$row['documento'];
+    	$ar['transportadora']['telefone']=$row['documento'];
+    	$ar['transportadora']['cpf/cnpj']=$row['documento'];
+    	$ar['transportadora']['ie']=$row['documento'];
+    	
+    	$ar['impostos']['bc_icms']=$row['documento'];
+    	$ar['impostos']['vr_icms']=$row['documento'];
+    	$ar['impostos']['bc_icms_st']=$row['documento'];
+    	$ar['impostos']['vr_icms_st']=$row['documento'];
+    	$ar['impostos']['total_prod']=$row['documento'];
+    	$ar['impostos']['vr_frete']=$row['documento'];
+    	$ar['impostos']['vr_seguro']=$row['documento'];
+    	$ar['impostos']['desconto']=$row['documento'];
+    	$ar['impostos']['despesas_acessorias']=$row['documento'];
+    	$ar['impostos']['vr_ipi']=$row['documento'];
+    	$ar['impostos']['vr_total_nota']=$row['documento'];
+    	 
+    	
+    	$ar['produtos'][]['cod']=$row['titulo'];
+    	$ar['produtos'][]['desc']=$row['titulo'];
+    	$ar['produtos'][]['ncm']=$row['titulo'];
+    	$ar['produtos'][]['cfop']=$row['titulo'];
+    	$ar['produtos'][]['und']=$row['titulo'];
+    	$ar['produtos'][]['qtd']=$row['titulo'];
+    	$ar['produtos'][]['vr_unitario']=$row['titulo'];
+    	$ar['produtos'][]['vr_desc']=$row['titulo'];
+    	$ar['produtos'][]['vr_total']=$row['titulo'];
+    	$ar['produtos'][]['bc_icms']=$row['titulo'];
+    	$ar['produtos'][]['vr_icms']=$row['titulo'];
+    	$ar['produtos'][]['vr_ipi']=$row['titulo'];
+    	$ar['produtos'][]['aliq_icms']=$row['titulo'];
+    	$ar['produtos'][]['aliq_ipi']=$row['titulo'];
 
-    private $server='';
-    private $user='';
-    private $pass='';
-    private $banco='';
-    private $database='';
-    private $sql=null;    
-    
-    
-    static function conectar(){
-      /*
-      * Método que conecta ao banco de dados passando
-      * os valores necessários para que a conexão ocorra
-      */
-      $conexao = mysql_connect($this->server,$this->user,$this->pass) or die($this->mensagem(mysql_error()));
-      return $conexao;
-   }
-    
-    static function selecionarDB(){
-      /*
-      * Método que seleciona o banco de dados
-      * com que irá trabalhar
-      */
- 
-      $banco = mysql_select_db($this->database) or die($this->mensagem(mysql_error()));
-      if($banco){
-         return true;
-      }else{
-         return false;
-      }
-   }
- 
-   static function executar(){
-      /*
-      * Método que executa uma query no banco de dados
-      */
-      $query = mysql_query($this->sql) or die ($this->mensagem(mysql_error()));
-      return $query;
-   }
-   
-   static function mensagem($erro){
-      /*
-      * Função para exibir os possíveis erros
-      * Separamos em um método, pois este pode ser estilizado,
-      * sem alterar outros métodos
-      */
-      echo $erro;
-   }
+    	$ar['info_complementares']=$row['documento'];
+    	
+    }
+    echo "<pre>";
+    echo print_r($ar);
+    echo "</pre>";
+    }
+    else {
+    	 die ("<h1>Falha na coneco com o Banco de Dados!</h1>");
 }
+
